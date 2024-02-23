c12701589c81945d7c364e2042d51a4917831c9f
Felipeleolim <161047774+Felipeleolim@users.noreply.github.com>
2024-02-23 15:31:17 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>RLX Apps</title>
+
+    <link rel="icon" href="public/RLX.ico" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <center>
+
+
+      
+        <a href="index.php">RLX Apps Homologados Security</a>
+      </center>
+      <footer>
+        <a href="login.php"> <button class="login-button">login</button> </a>
+      </footer>
+    </header>
+
+    <center>
+      <img class="logo" src="public/RLX APPS.png" />
+
+      <div class="container">
+        
+      <label for="username">Nome de Usuário:</label>
+  <input type="text" id="username" name="username" required>
+
+  <label for="password">Senha:</label>
+  <input type="password" id="password" name="password" required>
+
+  <input type="submit" value="Login">
+
+      </div>
+    </center>
+  </body>
+</html>
